bc3f73f14f4357714e67c0c81f7fba152a52b067
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-01-15 16:22:27 +0200

    comm

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+// const DB_HOST =
+//   "mongodb+srv://phoneBook:phoneBook@cluster0.auojrsw.mongodb.net/Phone_book_reader?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000  все гуд ");
-});
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection success");
+    app.listen(3002);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3002, () => {
+//   console.log("Server running. Use our API on port: 3000  все гуд ");
+// });
